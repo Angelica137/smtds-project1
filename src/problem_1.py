44e1194b19d930afc35ade55ff4226687f61db73
Angelica <Angelica137@users.noreply.github.com>
2024-07-03 17:32:46 +0100

    remove comments

--- src/problem_1.py
+++ src/problem_1.py
@@ -23,31 +23,3 @@ class LRU_Cache(object):
         elif len(self.cache) >= self.capacity:
             self.cache.popitem(last=False)
         self.cache[key] = value
-
-
-our_cache = LRU_Cache(5)
-
-our_cache.set(1, 1)
-our_cache.set(2, 2)
-our_cache.set(3, 3)
-our_cache.set(4, 4)
-
-
-our_cache.get(1)       # returns 1
-our_cache.get(2)       # returns 2
-our_cache.get(9)      # returns -1 because 9 is not present in the cache
-
-our_cache.set(5, 5)
-our_cache.set(6, 6)
-
-# returns -1 because the cache reached it's capacity and 3 was the least recently used entry
-our_cache.get(3)
-
-# Add your own test cases: include at least three test cases
-# and two of them must include edge cases, such as null, empty or very large values
-
-# Test Case 1
-
-# Test Case 2
-
-# Test Case 3
