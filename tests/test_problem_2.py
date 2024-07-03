6dcb812ae58bcaad7cb394b910698b8d701e71ee
Angelica <Angelica137@users.noreply.github.com>
2024-07-03 18:51:13 +0100

    test subdir returns correct content

--- tests/test_problem_2.py
+++ tests/test_problem_2.py
@@ -5,3 +5,9 @@ def test_empty_directory():
     no_match_files = "src/testdir/subdir2"
     result = find_files(".c", no_match_files)
     assert result == []
+
+
+def test_subdir5():
+    subdir5 = "src/testdir/subdir5"
+    result = find_files(".c", subdir5)
+    assert result == ["src/testdir/subdir5/a.c"]
