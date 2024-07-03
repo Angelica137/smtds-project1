c079df8f075100b2e251231a4a1d59e07181ad51
Angelica <Angelica137@users.noreply.github.com>
2024-07-03 18:40:30 +0100

    test no matching files, red

--- tests/test_problem_2.py
+++ tests/test_problem_2.py
@@ -1,2 +1,7 @@
 from src.problem_2 import find_files
 
+
+def test_empty_directory():
+    no_match_files = "src/testdir/subdir2"
+    result = find_files(".c", no_match_files)
+    assert result == []
