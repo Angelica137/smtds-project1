c7e3d1c9eefb23cd3a266fdfa0f710e9aa7b9e04
Angelica <Angelica137@users.noreply.github.com>
2024-07-28 13:25:34 +0100

    change test to assert

--- tests/test_problem_3.py
+++ tests/test_problem_3.py
@@ -3,5 +3,5 @@ from src.problem_3 import *
 
 def test_new_node():
     new_node = Node('a', 6)
-    new_node.char == 'a'
-    new_node.freq == 6
+    assert new_node.char == 'a'
+    assert new_node.freq == 6
