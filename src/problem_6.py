160903be8c74e7cf3f362e2839db52d4d2b0084c
Angelica <Angelica137@users.noreply.github.com>
2024-07-29 19:50:04 +0100

    crate union function, green

--- src/problem_6.py
+++ src/problem_6.py
@@ -11,6 +11,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    # string representation of an object
     def __str__(self):
         cur_head = self.head
         out_string = ""
@@ -20,7 +21,6 @@ class LinkedList:
         return out_string
 
     def append(self, value):
-
         if self.head is None:
             self.head = Node(value)
             return
@@ -42,8 +42,15 @@ class LinkedList:
 
 
 def union(llist_1, llist_2):
-    # Your Solution Here
-    pass
+    result = set()
+
+    # tuple packing
+    for llist in (llist_1, llist_2):
+        current = llist.head
+        while current:
+            result.add(current.value)
+            current = current.next
+    return result
 
 
 def intersection(llist_1, llist_2):
@@ -56,8 +63,8 @@ def intersection(llist_1, llist_2):
 linked_list_1 = LinkedList()
 linked_list_2 = LinkedList()
 
-element_1 = [3,2,4,35,6,65,6,4,3,21]
-element_2 = [6,32,4,9,6,1,11,21,1]
+element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 21]
+element_2 = [6, 32, 4, 9, 6, 1, 11, 21, 1]
 
 for i in element_1:
     linked_list_1.append(i)
@@ -65,16 +72,16 @@ for i in element_1:
 for i in element_2:
     linked_list_2.append(i)
 
-print (union(linked_list_1,linked_list_2))
-print (intersection(linked_list_1,linked_list_2))
+print(union(linked_list_1, linked_list_2))
+print(intersection(linked_list_1, linked_list_2))
 
 ## Test case 2
 
 linked_list_3 = LinkedList()
 linked_list_4 = LinkedList()
 
-element_1 = [3,2,4,35,6,65,6,4,3,23]
-element_2 = [1,7,8,9,11,21,1]
+element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 23]
+element_2 = [1, 7, 8, 9, 11, 21, 1]
 
 for i in element_1:
     linked_list_3.append(i)
@@ -82,8 +89,8 @@ for i in element_1:
 for i in element_2:
     linked_list_4.append(i)
 
-print (union(linked_list_3,linked_list_4))
-print (intersection(linked_list_3,linked_list_4))
+print(union(linked_list_3, linked_list_4))
+print(intersection(linked_list_3, linked_list_4))
 
 ## Add your own test cases: include at least three test cases
 ## and two of them must include edge cases, such as null, empty or very large values
---
+++ tests/test_problem_6.py
@@ -0,0 +1,25 @@
+import pytest
+from src.problem_6 import *
+
+
+@pytest.fixture
+def linked_lists():
+    linked_list_1 = LinkedList()
+    linked_list_2 = LinkedList()
+
+    element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 21]
+    element_2 = [6, 32, 4, 9, 6, 1, 11, 21, 1]
+
+    for i in element_1:
+        linked_list_1.append(i)
+
+    for i in element_2:
+        linked_list_2.append(i)
+
+    return linked_list_1, linked_list_2
+
+
+def test_union_returns_set():
+    new_union = union(linked_list_1, linked_list_2)
+    sorted_union = sorted(list(new_union))
+    assert sorted_union == [1, 2, 3, 4, 6, 9, 11, 21, 32, 35, 65]
