da5baca95bf60ad91ba97daabcdcdce53f3eef72
Angelica <Angelica137@users.noreply.github.com>
2024-07-29 14:18:57 +0100

    test null group

--- tests/test_problem_4.py
+++ tests/test_problem_4.py
@@ -1,3 +1,4 @@
+import pytest
 from src.problem_4 import *
 
 
@@ -21,3 +22,17 @@ def test_is_user_in_subgroup():
     user1 = "user1"
     sub_group.add_user(user1)
     assert is_user_in_group(user1, parent) is True
+
+
+def test_is_user_not_in_group():
+    parent = Group("parent")
+    sub_group = Group("sub_group")
+    parent.add_group(sub_group)
+    user1 = "user1"
+    assert is_user_in_group(user1, parent) is False
+
+
+def test_is_user_in_null_group():
+    # null group is = None
+    with pytest.raises(AttributeError):
+        is_user_in_group("user123", None)
