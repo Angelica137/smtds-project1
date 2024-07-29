e4316919459229ebe345867b58f2e9febe47d9b9
Angelica <Angelica137@users.noreply.github.com>
2024-07-29 14:05:10 +0100

    user in parent group, green

--- tests/test_problem_4.py
+++ tests/test_problem_4.py
@@ -4,4 +4,11 @@ from src.problem_4 import *
 def test_is_user_in_empty_group():
     empty = Group("empty")
     user1 = 'user1'
-    assert is_user_in_group('user1', empty) is False
+    assert is_user_in_group(user1, empty) is False
+
+
+def test_is_user_in_parent_group():
+    parent = Group("parent")
+    user1 = 'user1'
+    parent.add_user(user1)
+    assert is_user_in_group(user1, parent) is True
